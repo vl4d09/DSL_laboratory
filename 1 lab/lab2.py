44ed0510de788f63482f8e61f6fe3682e188be1e
vl4d09 <ungureanu.vlad19@gmail.com>
2024-03-07 17:45:19 +0200

    little changes

--- 1 lab/lab2.py
+++ 1 lab/lab2.py
@@ -179,20 +179,20 @@ grammar.P = {
 # Check the type of each grammar
 print("Grammar Classification:", grammar.check_grammar_type())
 
-# Define the finite automaton variant
+# Define the finite automaton variant ( states )
 finite_automaton = FiniteAutomaton()
-finite_automaton.Q = {'q0', 'q1', 'q2'}
-finite_automaton.Sigma = {'a', 'b', 'c'}
-finite_automaton.delta = {('q0', 'a', 'q0'), ('q0', 'b', 'q1'), ('q1', 'c', 'q1'),
-                          ('q1', 'c', 'q2'), ('q2', 'a', 'q0'), ('q1', 'a', 'q1')}
+finite_automaton.Q = {'q0', 'q1', 'q2', 'q3'}
+finite_automaton.Sigma = {'a', 'b'}
+finite_automaton.delta = {('q0', 'a', 'q1'), ('q1', 'b', 'q2'), ('q1', 'a', 'q3'),
+                          ('q0', 'b', 'q2'), ('q2', 'b', 'q3'), ('q1', 'a', 'q1')}
 finite_automaton.q0 = 'q0'
-finite_automaton.F = {'q2'}
+finite_automaton.F = {'q3'}
 
 # Convert finite automaton to regular grammar
 regular_grammar = finite_automaton.to_regular_grammar()
 
 # Print the regular grammar productions
-print("Regular Grammar Productions for the NDFA:")
+print("Conversion to grammar:")
 for non_terminal, productions in regular_grammar.P.items():
     for production in productions:
         print(non_terminal, "->", production)
@@ -214,4 +214,3 @@ if is_deterministic_dfa:
     print("The converted DFA is deterministic.")
 else:
     print("The converted DFA is non-deterministic.")
-     
